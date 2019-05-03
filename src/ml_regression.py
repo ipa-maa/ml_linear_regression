a43079c65457ca69586ddee65ac95083f621e9ee
Marcel Albus <marcel.albus@ipa.fraunhofer.de>
2019-05-03 16:38:57 +0200

    [CODE] update main

--- src/ml_regression.py
+++ src/ml_regression.py
@@ -13,14 +13,21 @@ History:
 - v1.0.0: first init
 """
 
-from misc import Font
-from misc import printy
-import numpy as np
-import tensorflow as tf
-from matplotlib import pyplot as plt
+#####################################
+#####################################
+import sys
+sys.path.append(".")
+#####################################
+#####################################
+# %%
+from ml_linear_regression.src.pointcloud import Pointcloud
 import sympy as sp
-
-from pointcloud import Pointcloud
+from matplotlib import pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from ml_linear_regression.src.misc import printy
+from ml_linear_regression.src.misc import Font
 
 
 class MLRegression:
@@ -32,9 +39,36 @@ class MLRegression:
                        'sigma': 17.0}
         self.y_rand = self._generate_points(**self.params)
         self.y = [self.params['m'] * x + self.params['c'] for x in range(len(self.y_rand))]
+        self.network = self._build_network()
 
     def _generate_points(self, m: float, c: float, size: int, sigma: float) -> np.array:
         return self.pc.generate_points(m=m, c=c, size=size, sigma=sigma)
+
+    def _build_network(self) -> keras.Model:
+        input_shape = (2,)
+        layer_input = keras.Input(shape=input_shape, name='layer_input')
+        layer_flatten = keras.layers.Flatten()(layer_input)
+        layer_dense = keras.layers.Dense(units=100, activation='relu', name='layer_dense')(layer_flatten)
+        out_m = keras.layers.Dense(units=1, activation='linear', name='out_m')(layer_dense)
+        out_c = keras.layers.Dense(units=1, activation='linear', name='out_c')(layer_dense)
+        optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9)
+        model = keras.Model(inputs=[layer_input], outputs=[out_m, out_c])
+        model.compile(optimizer=optimizer, loss=self._loss(m=self.params['m'], c=self.params['c']))
+        model.summary()
+        return model
+
+    def _loss(self, m: float, c: float):
+        """
+        y_true (tf tensor): target tensor
+        y_pred (tf tensor): model output tensor
+        """
+        K = keras.backend
+
+        def keras_loss(y_true, y_pred):
+            # y_true = keras.backend.placeholder(shape=(1,1))
+            y = [m, c]
+            return K.mean(K.square(y_true - y_pred))
+        return keras_loss
 
     def _calc_gradient(self):
         """
@@ -48,11 +82,9 @@ class MLRegression:
         plt.show()
 
 
-if __name__ == "__main__":
-    mlr = MLRegression()
-    mlr.main()
+# %%
+mlr = MLRegression()
+mlr.main()
 
-    x, y, z = sp.symbols('x, y, z')
-    sp.init_printing()
-    eq1 = x**2 + y
-    print(eq1.evalf(5, subs={x: 5.0, y: 2.0}))
+
+# %%
